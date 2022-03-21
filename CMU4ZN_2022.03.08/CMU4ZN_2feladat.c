2fbb656a4fadf40ec48e07b059b6772d1563fd25
CsonkaPatrik <99277973+CsonkaPatrik@users.noreply.github.com>
2022-03-21 13:39:15 +0100

    Add files via upload

---
+++ CMU4ZN_2022.03.08/CMU4ZN_2feladat.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+
+int main()
+{
+    char Input[50];
+    do{
+        scanf("ls" , Input);
+        system(Input);
+    }while(true);
+    return 0;
+}
